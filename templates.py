88ce10d283b9d230509137757f743308feff9201
Antonín Přikryl <assasinindy@gmail.com>
2022-04-24 22:42:33 +0200

    Add files via upload

---
+++ templates.py
@@ -0,0 +1,62 @@
+# this is just to make the code cleaner, so it's all centered here.
+
+prompt_templates = [
+  {
+      "prompt" : "{adjective} {thing}, {style}",
+  },
+  {
+     "prompt" :  "{adjective} {being}, {style}",
+  },
+  { 
+    "prompt" : "{color} {thing}, {style}"
+  },
+  {
+    "prompt" : "{color} {animal}, {style}"
+  },
+  {
+    "prompt" : "{adjective} {thing} by {artist}"
+  },
+  {
+    "prompt" : "{color} {being}, {style}"
+  },
+  {
+    "prompt" : "{adjective} {thing} in {location}"
+  },
+  {
+    "prompt" : "{adjective} {thing} of {of}"
+  },
+  {
+    "prompt" : "{color} {shape}, {style}"
+  },
+  {
+    "prompt" : "painting in style of {style} by {artist}"
+  },
+  {
+    "prompt" : "{thing} and {thing}, {style}"
+  },
+  {
+    "prompt" : "{thing} made out of {thing}s"
+  },
+  {
+    "prompt" : "goddess of {thing}, {style}"
+  },
+  {
+    "prompt" : "{thing}, {style}, {color} colour scheme"
+  },
+  {
+    "prompt" : "{being} {verb} {being}, {style}"
+  },
+  {
+    "prompt" : "anthropomorphic {animal}, {style}"
+  },
+  {
+    "prompt" : "{adjective} {being} {verb} {thing} in {location}, {style}"
+  },
+  {
+    "prompt" : "{color}-haired {being} {verb} {animal} in {location}, {style}"
+  },
+  {
+    "prompt" : "{adjective} {being} {int} in {location}"
+  }
+
+]
