6c48b6a9e24b99af5f9cbbf92503761499dfdfc0
Antonín Přikryl <assasinindy@gmail.com>
2022-04-29 00:59:20 +0200

    Update templates.py

--- templates.py
+++ templates.py
@@ -106,7 +106,7 @@ prompt_templates = [
   {"prompt" : "{being} wearing {adjective} {clothes}, {style}"},
   {"prompt" : "{animal} {intrans} in {location}, {style}"},
   {"prompt" : "an illustration of a {adjective} {being} holding a {thing}"},
-  {"prompt" : "a painting of a {location} in a {location}}"},
+  {"prompt" : "a painting of a {location} in a {location}"},
   {"prompt" : "a painting of {thing}s with {thing} {verb} the {thing}"}
   
 
